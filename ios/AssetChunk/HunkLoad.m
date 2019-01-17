ff2837bb048758bbd888eb32e8afefc48801a7a5
Beau Ner Chesluk <cmdr@beauner.com>
2019-01-17 13:28:52 +0000

    HTTP Req err handling

--- ios/AssetChunk/HunkLoad.m
+++ ios/AssetChunk/HunkLoad.m
@@ -9,64 +9,76 @@
 #import "SingleChunk.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 
+#define REQUEST_TIMEOUT 5.0
+#define INVOKE_FAKE_ERRORS 0
+
 @implementation HunkLoad
 
 -(id) initWithChunkRange:(long int)firstChunk to:(long int)lastChunk ownedBy:(ChunkAssetLoaderDelegate*)owner {
-    static int hunknum = 0;
     
     if (self = [super init]) {
         _firstChunk = firstChunk;
         _lastChunk = lastChunk;
         _owner = owner;
         _cancelled = false;
-        
-//        if (_firstChunk == _lastChunk) {
-//            NSLog(@"%li, // CHONK",_firstChunk);
-//        }
-        
-//        NSLog(@"HUNKY LOAD REQUESTED %i to %i",_firstChunk,_lastChunk);
-
-        NSURL * theUrl = owner.fileUrl;
-        
-//        hunknum++;
-//        if (hunknum == 100) {
-//            theUrl = [NSURL URLWithString:@"http://thepisspot.org/asfjkl"];
-//        }
-//        NSLog(@"HUNKNUM:%i%",hunknum);
-
-        NSMutableURLRequest *request =
-        [NSMutableURLRequest requestWithURL:theUrl
-                                cachePolicy:NSURLRequestUseProtocolCachePolicy
-                            timeoutInterval:60.0];
-    
         _nextByte = FirstByteOfChunk(_firstChunk);
-        
-        long int lastByte =LastByteOfChunk(_lastChunk);
-        if (owner.totalSize > -1) {
-            if (owner.totalSize <= lastByte) lastByte = owner.totalSize - 1;
-        }
-        
-        // Set the range for our request
-        NSString *range = @"bytes=";
-        range = [range stringByAppendingString:[[NSNumber numberWithLongLong:_nextByte]
-                                                stringValue]];
-        range = [range stringByAppendingString:@"-"];
-        range = [range
-                 stringByAppendingString:[[NSNumber numberWithLongLong:lastByte]
-                                          stringValue]];
-//        NSLog(@"HUNKY - range: %@", range);
-    
-        [request setValue:range forHTTPHeaderField:@"Range"];
-    
-        
-        _session = [NSURLSession sessionWithConfiguration:[HunkLoad getSessionConfig]
-                                                delegate:self
-                                           delegateQueue:[NSOperationQueue mainQueue]];
-        _task = [_session dataTaskWithRequest:request];
-        [_task resume];
+
+        [self makeTheRequest];
     }
     
     return self;
+}
+
+-(void)makeTheRequest {
+    
+    //        if (_firstChunk == _lastChunk) {
+    //            NSLog(@"%li, // CHONK",_firstChunk);
+    //        }
+    
+    //        NSLog(@"HUNKY LOAD REQUESTED %i to %i",_firstChunk,_lastChunk);
+    
+    NSURL * theUrl = _owner.fileUrl;
+    
+#if INVOKE_FAKE_ERRORS
+    static int hunknum = 0;
+            hunknum++;
+            if (hunknum % 40 == 0) {
+                theUrl = [NSURL URLWithString:@"http://thepisspot.org/asfjkl"];
+                NSLog(@"FUCKSAKE GONNA THROW THIS ONE");
+            }
+//            NSLog(@"FUCKSAKE HUNKNUM:%i",hunknum);
+#endif
+    
+    NSMutableURLRequest *request =
+    [NSMutableURLRequest requestWithURL:theUrl
+                            cachePolicy:NSURLRequestUseProtocolCachePolicy
+                        timeoutInterval:REQUEST_TIMEOUT];
+    
+    
+    long int lastByte =LastByteOfChunk(_lastChunk);
+    if (_owner.totalSize > -1) {
+        if (_owner.totalSize <= lastByte) lastByte = _owner.totalSize - 1;
+    }
+    
+    // Set the range for our request
+    NSString *range = @"bytes=";
+    range = [range stringByAppendingString:[[NSNumber numberWithLongLong:_nextByte]
+                                            stringValue]];
+    range = [range stringByAppendingString:@"-"];
+    range = [range
+             stringByAppendingString:[[NSNumber numberWithLongLong:lastByte]
+                                      stringValue]];
+    //        NSLog(@"HUNKY - range: %@", range);
+    
+    [request setValue:range forHTTPHeaderField:@"Range"];
+    
+    
+    _session = [NSURLSession sessionWithConfiguration:[HunkLoad getSessionConfig]
+                                             delegate:self
+                                        delegateQueue:[NSOperationQueue mainQueue]];
+    _task = [_session dataTaskWithRequest:request];
+    [_task resume];
+
 }
 
 -(void)cleanup {
@@ -136,6 +148,19 @@ dataTask:(NSURLSessionDataTask *)dataTask
 - (void)URLSession:(NSURLSession *)session
               task:(NSURLSessionTask *)task
 didCompleteWithError:(NSError *)error; {
+    if (error) {
+        NSLog(@"FUCKSAKE ERROR: %@ %@ %@",[error localizedDescription],[error localizedFailureReason],[error localizedRecoverySuggestion]);
+        if ((_session == session) && (_task == task) && (!_cancelled)) {
+            NSLog(@"FUCKSAKE GONNA TRY AGAIN");
+            [self cleanup];
+            _cancelled = false;
+
+            [self makeTheRequest];
+        } else {
+            NSLog(@"FUCKSAKE It's not me");
+        }
+        return;
+    }
     _owner = nil; // Hopefully garbage will get collected.
     [self cleanup];
 }
@@ -160,7 +185,7 @@ didCompleteWithError:(NSError *)error; {
     dispatch_once(&once, ^{
         sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
         
-        sessionConfig.timeoutIntervalForRequest = 30.0;
+        sessionConfig.timeoutIntervalForRequest = REQUEST_TIMEOUT;
         sessionConfig.HTTPMaximumConnectionsPerHost = 15;
         sessionConfig.HTTPCookieAcceptPolicy = NSHTTPCookieAcceptPolicyNever;
         sessionConfig.HTTPShouldUsePipelining = NO;
